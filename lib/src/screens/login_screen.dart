af7e0b72739059d806a5d13e505e7cb758f87c4e
Abraham <abrahamxjmv@gmail.com>
2019-03-28 22:26:13 -0700

    Primer practica formularios

---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  
+  createState() {
+    // TODO: implement createState
+    return LoginScreenState();
+  }
+}
+
+class LoginScreenState extends State<LoginScreen>{
+  Widget build(context) {
+    return Container(
+      margin:EdgeInsets.all(20.0),
+      child:Form(
+        child: Column(
+          children: <Widget>[],
+        ),
+      ),
+    );
+  }
+}
